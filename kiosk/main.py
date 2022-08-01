1b1c34b6db1cb7b8bbe9e17c236f3864c0d81172
민찬규 <cksrbals@gmail.com>
2022-08-01 17:06:29 +0900

    PYQT5 GUI 틀 까지만 제작 디바이스는 아직 연결 X

--- kiosk/main.py
+++ kiosk/main.py
@@ -8,6 +8,7 @@ win = tk.Tk()
 win.title("Hy-personal Booster Vender")
 win.geometry("1280x800")
 win.resizable(True, True)
+win['bg']='#FBCA53'
 
 #화면전환함수
 def change1to2():
@@ -24,8 +25,8 @@ def change3to4():
     label_subtext.configure(text='버튼을 클릭해주세요')
     label_maintext.configure(text='선택하신 프로틴이 맞는지 확인해 주세요')
     label_image.configure(image=image4,command=change2to3)
-    label_yesbutton.pack(side="left")
-    label_nobutton.pack(side="right")
+    label_yesbutton.pack(side="left",padx=70,pady=40)
+    label_nobutton.pack(side="right",padx=70,pady=40)
 def change4to5():
     label_subtext.configure(text='기다려주세요')
     label_maintext.configure(text='선택한 프로틴을 투하중입니다.')
@@ -44,23 +45,25 @@ def resetscreen():
     label_subtext.configure(text="투입하셨다면 클릭해주세요")
 
 #이미지파일
+#image0 = tkinter.PhotoImage(file="test.svg")
 image1 = tkinter.PhotoImage(file="tumblr.png")
 image2 = tkinter.PhotoImage(file="qrcode.png")
 image3 = tkinter.PhotoImage(file="server.png")
 image4 = tkinter.PhotoImage(file="protein.png")
 image5 = tkinter.PhotoImage(file="tumblrwithscoop.png")
 
+#label_mainpage = tkinter.Label(win, text="개인맞춤 운동 보조제 자판기\nHY-PERSONAL  BOOSTER VENDER\n", font=("HY헤드라인M",40))
 #초기화면
-label_maintext = tkinter.Label(win, text="투입구에 텀블러를 올려주세요", font=("나눔고딕",40))
-label_image = tkinter.Button(win, image=image1)
-label_subtext = tkinter.Label(win, text="투입하셨다면 클릭해주세요", font=("나눔고딕",40))
-label_image = tkinter.Button(win, image=image1, command=change1to2)
+label_maintext = tkinter.Label(win, text="투입구에 텀블러를 올려주세요", font=("나눔고딕",40), bg='#FBCA53')
+label_image = tkinter.Button(win, image=image1,bg='#FBCA53')
+label_subtext = tkinter.Label(win, text="투입하셨다면 클릭해주세요", font=("나눔고딕",40),bg='#FBCA53')
+label_image = tkinter.Button(win, image=image1, command=change1to2,bg='#FBCA53')
 
-label_maintext.pack(expand=1, anchor=CENTER)
-label_image.pack(expand=1, anchor=CENTER)
-label_subtext.pack(expand=1, anchor=CENTER)
-label_yesbutton=tkinter.Button(win, text="YES",command=change4to5,width=10,height=5)
-label_nobutton=tkinter.Button(win, text="NO", command=change2to3,width=10,height=5)
+label_maintext.pack(expand=1)
+label_image.pack(expand=1)
+label_subtext.pack(expand=1)
+label_yesbutton=tkinter.Button(win, text="YES",command=change4to5,bg='#FBCA53')
+label_nobutton=tkinter.Button(win, text="NO", command=change2to3,bg='#FBCA53')
 
 #실행
 win.mainloop()
--- kiosk/main_with_device.py
+++ kiosk/main_with_device.py
@@ -148,27 +148,30 @@ class Weightsensor:
 
 
 # endregion
+#region QR코드 관련 변수 설정
+global moctrl
+global usernumber
+global gram
+global motime
+#endregion
 # region 창설정
 win = tk.Tk()
 win.title("Hy-personal Booster Vender")
 win.geometry("1280x800")
 win.resizable(True, True)
-
-
 # endregion
-
 # region 화면전환함수
+#텀블러인식 -> qr코드인식
 def change1to2():
-    label_subtext.configure(text='인식시켰다면 화면을 클릭해 주세요')
+    label_subtext.configure(text='버튼을 눌러 qr코드 인식창을 띄워주세요')
     label_maintext.configure(text='카메라에 qr코드를 인식시켜 주세요')
     label_image.configure(image=image2, command=change2to3)
-
-
-global moctrl
-
-
+#qr코드인식 -> 서버통신
 def change2to3():
     global moctrl
+    global usernumber
+    global gram
+    global motime
     ap = argparse.ArgumentParser()
     ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
                     help="path to output CSV file containing barcodes")
@@ -178,15 +181,11 @@ def change2to3():
     time.sleep(2.0)
     csv = open(args["output"], "w")
     found = set()
-
     barcodeData = 0
     while True:
-
         frame = vs.read()
         frame = imutils.resize(frame, width=400)
-
         barcodes = pyzbar.decode(frame)
-
         for barcode in barcodes:
             (x, y, w, h) = barcode.rect
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -200,20 +199,18 @@ def change2to3():
                                            barcodeData))
                 csv.flush()
                 found.add(barcodeData)
-
         cv2.imshow("Barcode Scanner", frame)
         key = cv2.waitKey(1) & 0xFF
-
         if barcodeData != 0:
             time.sleep(1)
             break
 
     print("[INFO] cleaning up...")
     print(barcodeData)
-    usernumber = barcodeData[0:3]
+    usernumber = int(barcodeData[0:3])
     moctrl = int(barcodeData[3:5])
-    gram = barcodeData[5:]
-
+    gram = int(barcodeData[5:])
+    motime = gram/10
     csv.close()
     cv2.destroyAllWindows()
     vs.stop()
@@ -222,73 +219,65 @@ def change2to3():
     label_image.configure(image=image3, command=change3to4)
     label_yesbutton.pack_forget()
     label_nobutton.pack_forget()
-
-
+#서버통신 -> 프로틴확인절차, YES->프로틴투하, NO->다시서버와통신
 def change3to4():
     label_subtext.configure(text='버튼을 클릭해주세요')
     label_maintext.configure(text='선택하신 프로틴이 맞는지 확인해 주세요')
     label_image.configure(image=image4, command=change4to5)
     label_yesbutton.pack(side="left")
     label_nobutton.pack(side="right")
-
-
+#프로틴 투하 -> 투하완료
 def change4to5():
     label_subtext.configure(text='기다려주세요')
-    label_maintext.configure(text='투하중입니다.')
+    label_maintext.configure(text='버튼을 누르면 투하가 시작됩니다')
     label_image.configure(image=image1, command=actmotor)
     label_yesbutton.pack_forget()
     label_nobutton.pack_forget()
-
-
+#투하완료 -> 초기화면
 def change5to6():
     label_subtext.configure(text='감사합니다')
     label_maintext.configure(text='투하가 완료되었습니다')
     label_image.configure(command=resetscreen)
-
-
+#초기화면 -> 텀블러인식
 def resetscreen():
     label_maintext.configure(text="투입구에 텀블러를 올려주세요")
     label_image.configure(image=image1, command=change1to2)
     label_subtext.configure(text="투입하셨다면 클릭해주세요")
-
-
-def whichmotor(motorcontrol):
+#모터제어(번호,시간)
+def whichmotor(motorcontrol,motortime):
     if motorcontrol == 1:
         p1.start(0)
         p1.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p1.stop()
     elif motorcontrol == 2:
         p2.start(0)
         p2.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p2.stop()
     elif motorcontrol == 3:
         p3.start(0)
         p3.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p3.stop()
     elif motorcontrol == 4:
         p4.start(0)
         p4.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p4.stop()
     elif motorcontrol == 5:
         p5.start(0)
         p5.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p5.stop()
     elif motorcontrol == 6:
         p6.start(0)
         p6.ChangeDutyCycle(50)
-        time.sleep(1)
+        time.sleep(motortime)
         p6.stop()
-
-
 def actmotor():
-    whichmotor(moctrl)
+    whichmotor(moctrl,motime)
     change5to6()
-
 
 # endregion
 # region 이미지파일 및 초기설정
@@ -296,14 +285,13 @@ image1 = tkinter.PhotoImage(file="tumblr.png")
 image2 = tkinter.PhotoImage(file="qrcode.png")
 image3 = tkinter.PhotoImage(file="server.png")
 image4 = tkinter.PhotoImage(file="protein.png")
-image5 = tkinter.PhotoImage(file="tumblrwithscoop.png")
+#image5 = tkinter.PhotoImage(file="tumblrwithscoop.png")
 
 # 초기화면
 label_maintext = tkinter.Label(win, text="투입구에 텀블러를 올려주세요", font=("나눔고딕", 40))
 label_image = tkinter.Button(win, image=image1)
 label_subtext = tkinter.Label(win, text="투입하셨다면 클릭해주세요", font=("나눔고딕", 40))
 label_image = tkinter.Button(win, image=image1, command=change1to2)
-
 label_maintext.pack(expand=1, anchor=CENTER)
 label_image.pack(expand=1, anchor=CENTER)
 label_subtext.pack(expand=1, anchor=CENTER)

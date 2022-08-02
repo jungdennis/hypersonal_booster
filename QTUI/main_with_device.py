a8f9132c06d353a3a3ba7a16f43fbd6067ac019e
민찬규 <cksrbals@gmail.com>
2022-08-02 12:45:37 +0900

    PYQT5 GUI

--- QTUI/main_with_device.py
+++ QTUI/main_with_device.py
@@ -6,8 +6,197 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 import os
 import mouse
+import multiprocessing
+import RPi.GPIO as GPIO
+import time
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import argparse
+import datetime
+import imutils
+import time
+import cv2
 #endregion
+# region 모터설정
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
+motor1 = 20
+motor2 = 21
+'''
+motor3 = 12
+motor4 = 16
+motor5 = 7
+motor6 = 8
+'''
+GPIO.setup(motor1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(motor2, GPIO.OUT, initial=GPIO.LOW)
+'''
+GPIO.setup(motor3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(motor4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(motor5, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(motor6, GPIO.OUT, initial=GPIO.LOW)
+'''
+p1 = GPIO.PWM(motor1, 10)
+p2 = GPIO.PWM(motor2, 10)
+'''
+p3 = GPIO.PWM(motor3, 10)
+p4 = GPIO.PWM(motor4, 10)
+p5 = GPIO.PWM(motor5, 10)
+p6 = GPIO.PWM(motor6, 10)
+'''
+# endregion
+
+# region 무게센서 설정
+EMULATE_HX711 = False
+referenceUnit = 1
+if not EMULATE_HX711:
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+else:
+    from emulated_hx711 import HX711
+def cleanAndExit():
+    print("Cleaning...")
+    if not EMULATE_HX711:
+        GPIO.cleanup()
+    print("Bye!")
+    sys.exit()
+
+# region 무게 센서 세팅 및 초기 오차값
+# 텀블러, 원료통1~6세팅
+hx_main = HX711(26, 19)
+'''
+hx_1 = HX711(13, 6)
+hx_2 = HX711(5, 11)
+hx_3 = HX711(9, 10)
+hx_4 = HX711(22, 27)
+hx_5 = HX711(17, 4)
+hx_6 = HX711(3, 2)
+'''
+hx_main.set_reading_format("MSB", "MSB")
+hx_main.set_reference_unit(398)
+'''
+hx_1.set_reading_format("MSB", "MSB")
+hx_1.set_reference_unit(403)
+hx_2.set_reading_format("MSB", "MSB")
+hx_2.set_reference_unit(398)
+hx_3.set_reading_format("MSB", "MSB")
+hx_3.set_reference_unit(423)
+hx_4.set_reading_format("MSB", "MSB")
+hx_4.set_reference_unit(412)
+hx_5.set_reading_format("MSB", "MSB")
+hx_5.set_reference_unit(406)
+hx_6.set_reading_format("MSB", "MSB")
+hx_6.set_reference_unit(413)
+'''
+# endregion
+class Weightsensor:
+    def __init__(self):
+        self.count = 0
+
+    def weight_cup(self):
+        hx_main.reset()
+        hx_main.tare()
+        while True:
+            # 무게 측정
+            val_cup = round(hx_main.get_weight(5))
+            hx_main.power_down()
+            hx_main.power_up()
+            time.sleep(0.5)
+            print("컵 센서 측정값: %f" % val_cup)
+
+    '''
+    def weight_powder(self):
+        # region reset&tare
+        hx_1.reset()
+        hx_2.reset()
+        hx_3.reset()
+        hx_4.reset()
+        hx_5.reset()
+        hx_6.reset()
+        hx_1.tare()
+        hx_2.tare()
+        hx_3.tare()
+        hx_4.tare()
+        hx_5.tare()
+        hx_6.tare()
+        # endregion
+        while True:
+            # region getweight
+            val_1 = round(hx_1.get_weight(5), 1)
+            val_2 = round(hx_2.get_weight(5), 1)
+            val_3 = round(hx_3.get_weight(5), 1)
+            val_4 = round(hx_4.get_weight(5), 1)
+            val_5 = round(hx_5.get_weight(5), 1)
+            val_6 = round(hx_6.get_weight(5), 1)
+            # endregion
+            # region power_down_up
+            hx_1.power_down()
+            hx_2.power_down()
+            hx_3.power_down()
+            hx_4.power_down()
+            hx_5.power_down()
+            hx_6.power_down()
+
+            hx_1.power_up()
+            hx_2.power_up()
+            hx_3.power_up()
+            hx_4.power_up()
+            hx_5.power_up()
+            hx_6.power_up()
+            # endregion
+            time.sleep(0.1)
+            # region print_value
+            print("원료통1 측정값: %f" % val_1)
+            print("원료통2 측정값: %f" % val_2)
+            print("원료통3 측정값: %f" % val_3)
+            print("원료통4 측정값: %f" % val_4)
+            print("원료통5 측정값: %f" % val_5)
+            print("원료통6 측정값: %f" % val_6)
+            # endregion
+'''
+# endregion
+# region QR코드 관련 변수 설정
+global moctrl
+global usernumber
+global gram
+global motime
+# endregion
+# region DC모터 제어식
+def whichmotor(motorcontrol,motortime):
+    if motorcontrol == 1:
+        p1.start(0)
+        p1.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p1.stop()
+    elif motorcontrol == 2:
+        p2.start(0)
+        p2.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p2.stop()
+'''
+    elif motorcontrol == 3:
+        p3.start(0)
+        p3.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p3.stop()
+    elif motorcontrol == 4:
+        p4.start(0)
+        p4.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p4.stop()
+    elif motorcontrol == 5:
+        p5.start(0)
+        p5.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p5.stop()
+    elif motorcontrol == 6:
+        p6.start(0)
+        p6.ChangeDutyCycle(50)
+        time.sleep(motortime)
+        p6.stop()
+'''
+# endregion
 #region GUI 설정
 form_class = uic.loadUiType("uis/mainpage.ui")[0]
 global page_number
@@ -54,16 +243,63 @@ class WindowClass(QMainWindow, form_class):
             self.page_number += 1
         elif self.page_number == 1:
             self.stackedWidget.setCurrentIndex(2)
-            self.page_number += 1
-        elif self.page_number == 2:
+            time.sleep(1)
+            #region QR센서
+            global moctrl
+            global usernumber
+            global gram
+            global motime
+            ap = argparse.ArgumentParser()
+            ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
+                            help="path to output CSV file containing barcodes")
+            args = vars(ap.parse_args())
+            print("[INFO] starting video stream...")
+            vs = VideoStream(src=0).start()  # USB 웹캠 카메라 사용시
+            time.sleep(2.0)
+            csv = open(args["output"], "w")
+            found = set()
+            barcodeData = 0
+            while True:
+                frame = vs.read()
+                frame = imutils.resize(frame, width=400)
+                barcodes = pyzbar.decode(frame)
+                for barcode in barcodes:
+                    (x, y, w, h) = barcode.rect
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    barcodeData = barcode.data.decode("utf-8")
+                    barcodeType = barcode.type
+                    text = "{} ({})".format(barcodeData, barcodeType)
+                    cv2.putText(frame, text, (x, y - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    if barcodeData not in found:
+                        csv.write("{},{}\n".format(datetime.datetime.now(),
+                                                   barcodeData))
+                        csv.flush()
+                        found.add(barcodeData)
+                cv2.imshow("Barcode Scanner", frame)
+                key = cv2.waitKey(1) & 0xFF
+                if barcodeData != 0:
+                    time.sleep(1)
+                    break
+            print("[INFO] cleaning up...")
+            print(barcodeData)
+            usernumber = int(barcodeData[0:3])
+            moctrl = int(barcodeData[3:5])
+            gram = int(barcodeData[5:])
+            motime = gram / 10
+            csv.close()
+            cv2.destroyAllWindows()
+            vs.stop()
+            # endregion
             self.stackedWidget.setCurrentIndex(3)
-            self.page_number += 1
+            self.page_number = 3
         elif self.page_number == 3:
             self.stackedWidget.setCurrentIndex(4)
             self.page_number += 1
         elif self.page_number == 4:
             pass
         elif self.page_number == 5:
+            whichmotor(moctrl,motime)
             self.stackedWidget.setCurrentIndex(6)
             self.page_number += 1
         elif self.page_number == 6:
@@ -82,3 +318,15 @@ if __name__ == "__main__":
     myWindow = WindowClass()
     myWindow.show()
     app.exec_()
+    # 무게센서 클래스 지정
+    wei_cup = Weightsensor()
+    #    wei_powder = Weightsensor()
+    mp_gui = multiprocessing.Process(target=app.exec_)
+    mp_wei_cup = multiprocessing.Process(target=wei_cup.weight_cup)
+    #    mp_wei_powder = multiprocessing.Process(target=wei_powder.weight_powder)
+    mp_gui.start()
+    mp_wei_cup.start()
+    #    mp_wei_powder.start()
+    mp_gui.join()
+    mp_wei_cup.join()
+#    mp_wei_powder.join()

27e1c3a8b05229f16c43e8d1b3535fc0c5b5b800
jungdennis <syjung3355@gmail.com>
2022-08-16 23:39:09 +0900

    추천 알고리즘 작성중

--- app/app/src/main/java/com/example/hypersonalbooster/RecommendBooster.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/RecommendBooster.kt
@@ -1,48 +1,49 @@
 package com.example.hypersonalbooster
 
 class RecommendBooster() {
-    init {
-        // basic information
-        var age : Int
-        var sex : String
-        var pragent : Boolean
+    // basic information
+    var age : Int = 0
+    var sex : String = ""
+    var pragent : Boolean = false
 
-        // health information
-        var height : Float
-        var weight : Float
-        var fat : Float
-        var muscle : Float
+    // health information
+    var now_height : Float = 0.0F
+    var now_weight : Float = 0.0F
+    var now_fat : Float = 0.0F
+    var now_muscle : Float = 0.0F
 
-        // favorite information
+    // target information
+    var target_weight : Float = 0.0F
+    var target_fat : Float = 0.0F
+    var target_muscle : Float = 0.0F
 
+    // favorite information
 
-        // Particular information
-        var milk : Boolean
-        var vegan : Boolean
-        var caffeine : Boolean
-    }
+    // Particular information
+    var milk : Boolean = false
+    var vegan : Boolean = false
+    var caffeine : Boolean = false
 
-    // getter
-    fun get_basic(Age : Int, Sex: String, Pragent : Boolean) {
+    // setter
+    fun set_basic(Age : Int, Sex: String, Pragent : Boolean) {
         this.age = Age
         this.sex = Sex
         this.pragent = Pragent
     }
-    fun get_health(Height : Float, Weight : Float, Fat : Float, Muscle : Float) {
+    fun set_health(Height : Float, Weight : Float, Fat : Float, Muscle : Float) {
         this.now_height = Height
         this.now_weight = Weight
         this.now_fat = Fat
         this.now_muscle = Muscle
     }
-    fun get_target(Weight : Float, Fat : Float, Muscle : Float) {
-        this.now_weight = Weight
-        this.now_fat = Fat
-        this.now_muscle = Muscle
+    fun set_target(Weight : Float, Fat : Float, Muscle : Float) {
+        this.target_weight = Weight
+        this.target_fat = Fat
+        this.target_muscle = Muscle
     }
-    fun get_particular(Vegan : Boolean, Milk : Boolean, Caffeine : Boolean) {
+    fun set_particular(Vegan : Boolean, Milk : Boolean, Caffeine : Boolean) {
         this.vegan = Vegan
         this.milk = Milk
         this.caffeine = Caffeine
     }
-
 }

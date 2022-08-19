aba4da973cef7a5d01cfae9a6956146f0a0c22b0
unknown <superlove133@gmail.com>
2022-08-19 10:56:31 +0900

    anko 의존성 제거 및 내 위치 버튼 수정

--- app/app/src/main/java/com/example/hypersonalbooster/BoosterFragment_After.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/BoosterFragment_After.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.hypersonalbooster.databinding.LayoutBoosterBeforeBinding
-
+/*
 class BoosterFragment_After : Fragment() {
 
     override fun onCreateView(
@@ -31,3 +31,6 @@ class BoosterFragment_After : Fragment() {
             ContextCompat.startActivity(intent)
         }
 }
+
+*/
+

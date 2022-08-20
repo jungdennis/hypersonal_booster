67d049d323a23991d956964d00feede5b4a9fddc
jungdennis <syjung3355@gmail.com>
2022-08-20 18:47:37 +0900

    서버 데이터 받기 작업중

--- app/app/src/main/java/com/example/hypersonalbooster/RegisterActivity_0_Welcome.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/RegisterActivity_0_Welcome.kt
@@ -2,15 +2,27 @@ package com.example.hypersonalbooster
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hypersonalbooster.databinding.LayoutRegisterWelcomeBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlin.properties.Delegates
 
 class RegisterActivity_0_Welcome : AppCompatActivity() {
 
     private lateinit var binding : LayoutRegisterWelcomeBinding
 
+    val database = FirebaseDatabase.getInstance("https://hypersonal-booster-default-rtdb.asia-southeast1.firebasedatabase.app")
+    val ref = database.getReference("1RwUEzmqz5l9hilFIeJI5gEQu3AUwRAepCc4YzzJGnZY/booster")
+
     private var end_time: Long = 0
+
+    var company_list = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,30 @@ class RegisterActivity_0_Welcome : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity_1_Basic::class.java)
             startActivity(intent)
         }
+
+        Thread{
+            var data_length : Long = 0
+
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    data_length = dataSnapshot.childrenCount
+                    for (snapshot in dataSnapshot.getChildren()) {
+                        val brand = snapshot.child("brand").getValue().toString()
+                        if(brand.isNotEmpty()) {
+                            company_list.add(brand)
+                            Log.d("RegisterActiviy_0_Welcome", "found : " + snapshot.child("brand").getValue() + " / brand : " + brand)
+                        }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+        }.run()
+
+        // do{ } while(company_list.size.toLong() != data_length)
+
+        val print = company_list.size
+        Log.d("RegisterActiviy_0_Welcome", "length : $print")
     }
 
     override fun onBackPressed() {
@@ -30,6 +66,8 @@ class RegisterActivity_0_Welcome : AppCompatActivity() {
         if (System.currentTimeMillis() - end_time >= 2000) {
             end_time = System.currentTimeMillis()
             Toast.makeText(applicationContext, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            val print = company_list.size
+            Log.d("RegisterActiviy_0_Welcome", "length : $print")
         } else if (System.currentTimeMillis() - end_time < 2000) {
             finishAffinity()
         }
--- app/app/src/main/java/com/example/hypersonalbooster/RegisterActivity_8_Company.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/RegisterActivity_8_Company.kt
@@ -2,18 +2,32 @@ package com.example.hypersonalbooster
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hypersonalbooster.databinding.LayoutRegisterCompanyBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class RegisterActivity_8_Company : AppCompatActivity() {
 
     private lateinit var binding : LayoutRegisterCompanyBinding
+
+    val database = FirebaseDatabase.getInstance("https://hypersonal-booster-default-rtdb.asia-southeast1.firebasedatabase.app")
+    val ref = database.getReference("1RwUEzmqz5l9hilFIeJI5gEQu3AUwRAepCc4YzzJGnZY")
+
+    var company_list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = LayoutRegisterCompanyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        seek_brand()
 
         binding.back.setOnClickListener {
             finish()
@@ -64,4 +78,22 @@ class RegisterActivity_8_Company : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun seek_brand() {
+        val booster_database = ref.child("booster")
+        booster_database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    val brand = snapshot.child("brand").toString()
+                    if(brand.isNotEmpty()) {
+                        company_list.add(brand)
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {} })
+
+        val length = company_list.size
+        Toast.makeText(this, "$length", Toast.LENGTH_SHORT).show()
+    }
+
 }

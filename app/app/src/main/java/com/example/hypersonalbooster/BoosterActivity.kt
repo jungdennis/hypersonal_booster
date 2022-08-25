3ce6aa9e9bd7c6a3e9d3e22a24d15276db40c740
joonhoflow <87225193+joonhoflow@users.noreply.github.com>
2022-08-25 13:02:28 +0900

    보충제 아이디 넣기

--- app/app/src/main/java/com/example/hypersonalbooster/BoosterActivity.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/BoosterActivity.kt
@@ -1,26 +1,53 @@
 package com.example.hypersonalbooster
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hypersonalbooster.databinding.LayoutBoosterMainBinding
-import android.widget.Button
-import android.widget.Toast
-import com.example.hypersonalbooster.databinding.LayoutBoosterAfterBinding
-import com.example.hypersonalbooster.databinding.LayoutBoosterBeforeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class BoosterActivity : AppCompatActivity() {
 
     private lateinit var binding : LayoutBoosterMainBinding
 
+    val database = FirebaseDatabase.getInstance("https://hypersonal-booster-default-rtdb.asia-southeast1.firebasedatabase.app")
+    val ref = database.getReference("1RwUEzmqz5l9hilFIeJI5gEQu3AUwRAepCc4YzzJGnZY")
+
+
     private var end_time: Long = 0
+
+    var booster_before = ArrayList<Booster>()
+    var booster_after = ArrayList<Booster>()
+    var booster_init = ArrayList<Booster>()
+
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = LayoutBoosterMainBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
+        val shared_cloud = getSharedPreferences("data_cloud", 0)
+        val name = shared_cloud.getString("name", "닉네임없음")
+        val before = shared_cloud.getString("booster_before", "NoBooster")!!.split(",")
+        val after = shared_cloud.getString("booster_after", "NoBooster")!!.split(",")
+        val uid = shared_cloud.getString("uid", "NoUid")
+
+        binding.userName.text = name
 
         setContentView(binding.root)
+
+        for(boosterID in before) {
+            booster_before.add(Booster(boosterID))
+        }
+        for(boosterID in after) {
+            booster_after.add(Booster(boosterID))
+        }
+        for(boosterID in before) {
+            booster_init.add(Booster(boosterID))
+        }
 
         binding.moreBefore.setOnClickListener {
             val intent = Intent(this, BoosterActivity_Before :: class.java)

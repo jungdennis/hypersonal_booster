e90542b07e6ed0bc143aeb82956ca974fa7319f1
jungdennis <syjung3355@gmail.com>
2022-08-27 17:59:46 +0900

    Booster 클래스 업데이트 (Booster 종류 세분화 표시 및 추가 정보 수집)

--- app/app/src/main/java/com/example/hypersonalbooster/Booster.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/Booster.kt
@@ -10,6 +10,7 @@ class Booster(id : String) {
     val database = FirebaseDatabase.getInstance("https://hypersonal-booster-default-rtdb.asia-southeast1.firebasedatabase.app")
     val ref = database.getReference("1RwUEzmqz5l9hilFIeJI5gEQu3AUwRAepCc4YzzJGnZY/booster")
 
+    // 데이터베이스로 주로 사용할 변수들
     var name : String = ""              // 이름
     var class_0 : String = ""           // 분류0 : 가루, 액체, 그외
     var class_1 : String = ""           // 분류1 : 운동 전/후
@@ -25,16 +26,27 @@ class Booster(id : String) {
     var link : String = ""              // 구매링크
     var etc : String = ""               // 특이사항
 
+    // 엥간하면 사용할 일 없는 변수들
+    var company : String = ""           // 회사
+    //var taste_1 = ArrayList<String>()   // 맛1 : 대분류
+    //var taste_2 : ArrayList<String>()   // 맛2 : 상세분류
+    var texture : String = ""           // 느낌
+
     init {
         ref.child(id).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 name = snapshot.child("name").getValue().toString()
                 Log.d("Booster Init", "Start Init : $name")
 
+                texture = snapshot.child("texture").getValue().toString()
+                company = snapshot.child("brand").getValue().toString()
+                Log.d("Booster Init", "Texture : $texture")
+
                 var temp_0 = snapshot.child("class0(가루0액체1그외2)").getValue()
                 var temp_1 = snapshot.child("class1(전0후1)").getValue()
                 var temp_2 = snapshot.child("class2(BCAA0부스터류1)(게이너0그외1)").getValue()
-                Log.d("Booster Init", "read complete : $temp_0, $temp_1, $temp_2")
+                var temp_3 = snapshot.child("class3(카페인x0카페인o1)(WPC0WPI1WPH2비건3카제인4)").getValue()
+                Log.d("Booster Init", "read complete : $temp_0, $temp_1, $temp_2, $temp_3")
 
                 if(temp_0 == 0L) {
                     class_0 += "powder"
@@ -48,20 +60,47 @@ class Booster(id : String) {
 
                 if(temp_1 == 0L) {
                     class_1 += "before"
+
                     if(temp_2 == 0L) {
-                        class_2 += "BCAA"
+                        if(temp_3 == 0L) {
+                            class_2 += "무카페인 BCAA"
+                        }
+                        else if(temp_3 == 1L) {
+                            class_2 += "BCAA"
+                        }
                     }
                     else if(temp_2 == 1L) {
-                        class_2 += "부스터"
+                        if(temp_3 == 0L) {
+                            class_2 += "무카페인 부스터"
+                        }
+                        else if(temp_3 == 1L) {
+                            class_2 += "부스터"
+                        }
                     }
                 }
                 else if(temp_1 == 1L) {
                     class_1 += "after"
+
                     if(temp_2 == 0L) {
                         class_2 += "게이너"
                     }
                     else if(temp_2 == 1L) {
-                        class_2 += "프로틴"
+                        if(texture == "thick") {
+                            if(temp_3 == 1L || temp_3 == 2L) {
+                                class_2 += "분리 유청 프로틴"
+                            }
+                            else if(temp_3 == 0L || temp_3 == 3L) {
+                                class_2 += "프로틴"
+                            }
+                        }
+                        else if(texture == "clear") {
+                            if(temp_3 == 1L || temp_3 == 2L) {
+                                class_2 += "분리 유청 클리어웨이"
+                            }
+                            else if(temp_3 == 0L || temp_3 == 3L) {
+                                class_2 += "클리어웨이"
+                            }
+                        }
                     }
                 }
                 Log.d("Booster Init", "Sort Complete : $class_0, $class_1, $class_2")

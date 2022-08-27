45fb3a870edd502580c28fa03719831548957b7a
jungdennis <syjung3355@gmail.com>
2022-08-28 00:55:43 +0900

    보충제 사진 2차 수정

--- app/app/src/main/java/com/example/hypersonalbooster/ListViewAdapter_Main.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/ListViewAdapter_Main.kt
@@ -1,20 +1,16 @@
 package com.example.hypersonalbooster
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.Toast
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
+import android.widget.ImageView
+import coil.api.load
 import com.example.hypersonalbooster.databinding.FragmentMainBoosterAdapterBinding
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 
 class ListViewAdapter_Main(private val context: Context, private val booster_list : ArrayList<Booster>)
@@ -31,25 +27,20 @@ class ListViewAdapter_Main(private val context: Context, private val booster_lis
         val booster = booster_list[position]
 
         val image_name = booster.name + ".jpg"
-        val image_path = FirebaseStorage.getInstance("gs://hypersonal-booster.appspot.com").getReference()
+        val image_path : StorageReference = FirebaseStorage.getInstance("gs://hypersonal-booster.appspot.com").reference.child(image_name)
 
         binding.boosterKind.text = booster.class_2
         binding.boosterName.text = booster.name
 
-        val url = image_path.child(image_name).downloadUrl
+        val booster_url_name = booster.name.replace("%", "%25").replace(" ", "%20").replace("+","%2B")
+
+        val url = "https://firebasestorage.googleapis.com/v0/b/hypersonal-booster.appspot.com/o/" + booster_url_name + ".jpg?alt=media"
         Log.d("ListViewAdapter_Main", "Url : $url")
 
-        image_path.child(image_name).getDownloadUrl().addOnCompleteListener( { task ->
-            if (task.isSuccessful) {
-                val requestOptions: RequestOptions = RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
-
-                Glide.with(context).load(task.getResult()).apply(requestOptions).into(binding.boosterImage)
-            } else {
-                val booster_image = context.resources.getIdentifier("img_scoop", "drawable", context.packageName)
-
-                binding.boosterImage.setImageResource(booster_image)
-            }
-        })
+        binding.boosterImage.load(url) {
+            placeholder(R.drawable.icon_loading)
+            error(R.drawable.img_scoop)
+        }
 
         return binding.root
     }

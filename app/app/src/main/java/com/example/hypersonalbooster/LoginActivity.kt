4d45e58b3dc422cc892dc96e4a950d112cb21de1
jungdennis <syjung3355@gmail.com>
2022-08-27 13:32:17 +0900

    자동 로그인 구현중

--- app/app/src/main/java/com/example/hypersonalbooster/LoginActivity.kt
+++ app/app/src/main/java/com/example/hypersonalbooster/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.hypersonalbooster
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,10 +20,10 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), CloudCallbackListener {
 
     private lateinit var binding : LayoutLoginBinding
 
@@ -34,6 +35,9 @@ class LoginActivity : AppCompatActivity() {
     val ref = database.getReference("members")
 
     private var end_time: Long = 0
+
+    var check_health = ""
+    var check_cloud = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,83 +98,91 @@ class LoginActivity : AppCompatActivity() {
                     val uid = user!!.uid.toString()
                     val name = user!!.displayName
 
-                    val cloud = cloud_check(uid)
-                    val health = health_check()
-
                     ref.child(uid).child("UID").setValue(uid)
                     ref.child(uid).child("name").setValue(name)
 
-                    val shared = getSharedPreferences("data_cloud", 0)
-                    val editor = shared.edit()
+                    val shared_cloud = getSharedPreferences("data_cloud", 0)
+                    val editor_cloud = shared_cloud.edit()
 
-                    editor.putString("uid", uid)
-                    editor.putString("name", name)
-                    editor.apply()
-
+                    editor_cloud.putString("uid", uid)
+                    editor_cloud.putString("name", name)
+                    editor_cloud.apply()
 
                     Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT)
                         .show()
 
-                    if(cloud) {
-                        if(health) {
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                        else {
-                            val intent = Intent(this, RegisterActivity_0_Welcome::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                    }
-                    else{
-                        val intent = Intent(this, RegisterActivity_0_Welcome::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-                else {
+                    health_check()
+                    cloud_check(uid, this)
+
+
+                } else {
                     Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT)
                         .show()
                 }
             }
     }
 
-    private fun health_check() : Boolean {
-        val check = getSharedPreferences("data_health", 0)
-        val editor = check.edit()
+    private fun health_check() {
+        val shared_health = getSharedPreferences("data_health", 0)
+        val editor_health = shared_health.edit()
 
-        val height = check.getFloat("height", 0.0F)
-        val weight = check.getFloat("weight", 0.0F)
+        val height = shared_health.getFloat("height", 0.0F)
+        val weight = shared_health.getFloat("weight", 0.0F)
 
-        if(height == 0.0F || weight == 0.0F) {
+        if (height == 0.0F || weight == 0.0F) {
             Log.d("health_check", "false")
-            return false
-        }
-        else {
-            Log.d("health_check", "true")
-            editor.putString("health_check", "true")
-            editor.apply()
-            return true
+            check_health = "false"
+        } else {
+            Log.d("check_health", "true")
+            editor_health.putString("check_health", "true")
+            editor_health.apply()
+            check_health = "true"
         }
     }
 
-    fun cloud_check(uid : String) : Boolean {
-        var flag : String? = null
+    private fun cloud_check(uid : String, callback : CloudCallbackListener) {
+        var flag: String = ""
+        ref.child(uid).child("cloud_check")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot != null) {
+                        Log.d("check_cloud", "snapshot result : $dataSnapshot")
+                        if (dataSnapshot.value != null) {
+                            Log.d("check_cloud", "true")
+                            check_cloud = "true"
+                        }
+                        else {
+                            Log.d("check_cloud", "false")
+                            check_cloud = "false"
+                        }
 
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                flag = dataSnapshot.child("cloud_flag").getValue().toString()
+                        callback.onFlag()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+    }
+
+    override fun onFlag() {
+        if(check_cloud == "true") {
+            getSharedPreferences("data_cloud", 0).edit().putString("cloud_check", "true").apply()
+            if(check_health == "true") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
             }
-            override fun onCancelled(databaseError: DatabaseError) {}})
-
-        if(flag == null) {
-            Log.d("cloud_check", "false")
-            return false
+            else {
+                val intent = Intent(this, RegisterActivity_0_Welcome::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
         else{
-            Log.d("cloud_check", "true")
-            return true
+            getSharedPreferences("data_cloud", 0).edit().putString("cloud_check", "false").apply()
+            val intent = Intent(this, RegisterActivity_0_Welcome::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
